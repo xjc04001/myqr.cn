d87f81ae54ee39a0fa1f7074211d4627acde9d46
谢计春 <weixin@2id.cn>
2026-08-12 14:31:43 +0800

    update footer links and git command notes

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -2,9 +2,9 @@
   <footer class="site-footer">
     <p class="record-line">
       <img src="/images/ghs.png" alt="" />
-      <span>苏公网安备 32010202011198号</span>
+      <span><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32010202011198">苏公网安备 32010202011198号</a></span>
     </p>
-    <p>苏ICP备14035966号</p>
+    <p><a target="_blank" href="https://beian.miit.gov.cn/">苏ICP备14035966号</a></p>
     <p>Copyright © 2026 智联码云平台</p>
   </footer>
 </template>
